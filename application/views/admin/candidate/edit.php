a4ecabe68729c3eee63eb064411e3247e4318ace
longnd6 <longndbkat@gmail.com>
2017-12-01 17:27:36 +0700

    update phần edit candidate

--- application/views/admin/candidate/edit.php
+++ application/views/admin/candidate/edit.php
@@ -7,13 +7,13 @@
 				</li>
 				<li>
 					<i class="icon-edit"></i>
-					<a href="#">Sửa tài khoản quản trị</a>
+					<a href="#">Sửa thông tin ứng viên</a>
 				</li>
 			</ul>          
 			<div class="row-fluid sortable">
 				<div class="box span12">
 					<div class="box-header" data-original-title>
-						<h2><i class="halflings-icon edit"></i><span class="break"></span>Thông tin quản trị viên</h2>
+						<h2><i class="halflings-icon edit"></i><span class="break"></span>Thông tin chung</h2>
 						<div class="box-icon">
 							<a href="#" class="btn-setting"><i class="halflings-icon wrench"></i></a>
 							<a href="#" class="btn-minimize"><i class="halflings-icon chevron-up"></i></a>
@@ -29,6 +29,14 @@
 								<input type="text" class="span6 typeahead" name="full_name" value="<?php echo $info->full_name; ?>" >
 								<div class="help-block"><?php echo form_error('full_name'); ?></div>
 							  </div>
+
+								<div class="control-group">
+							  <label class="control-label" for="typeahead">Ngày sinh </label>
+							  <div class="controls">
+								<input type="text" class="span6 typeahead" name="birthday" value="<?php echo date_format(new DateTime($info->birthday),'d/m-Y'); ?>" >
+								<div class="help-block"><?php echo form_error('birthday'); ?></div>
+							  </div>
+
 							</div>					
 							<div class="control-group">
 							  <label class="control-label" for="fileInput">Ảnh đại diện</label>
@@ -36,7 +44,30 @@
 								<input class="input-file uniform_on" id="fileInput" type="file">
 							  </div>
 							</div>  
-
+							<div class="control-group">
+							  <label class="control-label">Giới tính</label>
+							  <div class="controls">
+								<label class=" control-label">
+									<input type="radio" name="gender" value="0" <?php if ($info->gender==0) echo "checked";?>>Nữ									
+								</label>
+								<label class=" control-label">
+									<input type="radio" name="gender" value="1" <?php if ($info->gender==1) echo "checked";?>>Nam
+								</label>
+								<label class=" control-label">
+									<input type="radio" name="gender" value="3" <?php if ($info->gender==3) echo "checked";?>>Khác
+								</label>
+							  </div>
+							</div>
+							<div class="control-group">
+							<label class="control-label">Thành phố</label>
+							<div class="controls">
+							<select name="city">											
+												<?php foreach($lst_city as $row): ?>
+												<option value="<?php echo $row->id; ?>" <?php if ($info->city_id==$row->id) echo "selected";?>><?php echo $row->name; ?></option>
+												<?php endforeach; ?>
+											</select>
+							</div>
+							</div>
 							<div class="control-group">
 							  <label class="control-label" for="typeahead">Địa chỉ</label>
 							  <div class="controls">
@@ -50,11 +81,22 @@
 								<input type="text" class="span6 typeahead" name="phone" value="<?php echo $info->phone; ?>" >
 							  </div>
 							</div>
-
+							<div class="control-group">
+							  <label class="control-label" for="typeahead">Giới thiệu bản thân</label>
+							  <div class="controls">
+								<textarea class="form-control span6" rows="5" id="description" name="description"><?php echo $info->description; ?></textarea>
+							  </div>
+							</div>
+							<div class="control-group">
+							  <label class="control-label" for="date01">Ngày sửa</label>
+							  <div class="controls">
+								<input type="text" class="input-xlarge" id="date01" value="<?php echo $info->modified_date; ?>" readonly="true">
+							  </div>
+							</div>
 							<div class="control-group">
 							  <label class="control-label" for="date01">Ngày tạo</label>
 							  <div class="controls">
-								<input type="text" class="input-xlarge datepicker" id="date01" value="<?php echo $info->created_date; ?>">
+								<input type="text" class="input-xlarge" id="date01" value="<?php echo $info->created_date; ?>" readonly="true">
 							  </div>
 							</div>
 							<div class="form-actions">
