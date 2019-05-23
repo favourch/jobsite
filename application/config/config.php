2f503ab7de85518b2f9938e1090670be0478db9a
SenseiFavour <fchukwuedo@gmail.com>
2019-05-23 20:40:34 +0100

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ $config['root_admin'] = 6;
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/jobsite/';
+$config['base_url'] = 'https://internusng.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
